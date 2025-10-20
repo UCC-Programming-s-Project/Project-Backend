69956332183cc5d172e72aff62be2c2dd60e4a2f
DigitalsMau <maury.digitals@gmail.com>
2025-10-19 21:47:03 -0500

    Versión inicial del proyecto Alquiler de Mangas

---
+++ alquilermangas/src/main/java/com/proyectoucc/alquilermangas/ProyectoUccAlquilermangasApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoucc.alquilermangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUccAlquilermangasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUccAlquilermangasApplication.class, args);
+	}
+
+}
